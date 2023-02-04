090bcf1489d44726b885173b81a50a98790aff6d
Jaanus Kase <jaanus@gmail.com>
2023-02-04 12:07:16 +0200

    Initial Commit

---
+++ Thoughts/ThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtsApp.swift
+//  Thoughts
+//
+//  Created by Jaanus Kase on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
