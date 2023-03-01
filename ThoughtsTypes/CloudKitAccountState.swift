06aec969ca22ed41fb668d20764600184ce94d62
Jaanus Kase <jaanus@gmail.com>
2023-03-01 14:15:32 +0200

    Clarifying comments.

--- ThoughtsTypes/CloudKitAccountState.swift
+++ ThoughtsTypes/CloudKitAccountState.swift
@@ -2,7 +2,8 @@ public enum CloudKitAccountState: Codable {
   /// CloudKit account is operational and available.
   case available
   
-  /// We haven’t yet checked the state, but optimistically assume it is available.
+  /// We optimistically assume the account is available, after we have previously
+  /// initialized the app state.
   ///
   /// This is to avoid UI flickering in the case of “happy path”, where the account indeed
   /// is available but there can be a slight delay at the start of the app while it is checked.
@@ -13,6 +14,7 @@ public enum CloudKitAccountState: Codable {
   /// In a real app, you would distinguish more between the various CloudKit statuses.
   case noAccount
   
-  /// Undetermined account state.
+  /// Undetermined account state. This is the initial state when the app is first started
+  /// on this device.
   case unknown
 }
