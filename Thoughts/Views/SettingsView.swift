13775dd2293bf3ee18f7187e81625ab3670618e9
Jaanus Kase <jaanus@gmail.com>
2023-03-01 14:43:22 +0200

    SettingsView UI

--- Thoughts/Views/SettingsView.swift
+++ Thoughts/Views/SettingsView.swift
@@ -1,25 +1,28 @@
-//
-//  SettingsView.swift
-//  Thoughts
-//
-//  Created by Jaanus Kase on 01.03.2023.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
-    var body: some View {
-      VStack {
-        Spacer()
-        Text("Hello, settings")
-          .frame(maxWidth: .infinity)
-      }
-      .padding()
+  var body: some View {
+    VStack(alignment: .leading, spacing: 16) {
+      #if os(iOS)
+      Text("Settings")
+        .font(.largeTitle)
+        .bold()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.top)
+      #endif
+      Spacer()
+      Button(action: {}, label: {
+        Text("Reset local cache")
+      })
+      Text("Reset the cache to clear local app state and re-download everything from iCloud.")
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
+    .padding()
+  }
 }
 
 struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-    }
+  static var previews: some View {
+    SettingsView()
+  }
 }
---
+++ Thoughts/Views/SettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewModel.swift
+//  Thoughts
+//
+//  Created by Jaanus Kase on 01.03.2023.
+//
+
+import Foundation
